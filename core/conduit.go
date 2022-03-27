26190d59eab242b7cd85dfa8e79429c269f1c647
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2022-03-27 15:37:04 -0400

    Refactored Main()

--- core/conduit.go
+++ core/conduit.go
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2015-2018 Lightning Labs and The Lightning Network Developers
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 package core
 
 import (
@@ -9,6 +27,7 @@ import (
 
 	"github.com/TheRebelOfBabylon/Conduit/errors"
 	"github.com/TheRebelOfBabylon/Conduit/intercept"
+	e "github.com/pkg/errors"
 	"github.com/rs/zerolog"
 )
 
@@ -55,28 +74,43 @@ func parseLndLog(scan *bufio.Scanner, log *zerolog.Logger, re *regexp.Regexp, sh
 // Main is the true entry point for Conduit
 func Main(shutdownInterceptor *intercept.Interceptor, cfg *Config, log zerolog.Logger) error {
 	var wg sync.WaitGroup
+	// starting LND
+	_, err := startLnd(cfg, wg, &log, shutdownInterceptor)
+	if err != nil {
+		err = e.Wrap(err, "could not start lnd")
+		log.Fatal().Msg(err.Error())
+		return err
+	}
+	<-shutdownInterceptor.ShutdownChannel()
+	wg.Wait()
+	return nil
+}
+
+// startLnd starts LND if it's been installed with a given config
+func startLnd(_ *Config, wg sync.WaitGroup, log *zerolog.Logger, shutdownInterceptor *intercept.Interceptor) (*bufio.Scanner, error) {
 	// Let's check if LND is installed
 	if _, err := exec.LookPath("lnd"); err != nil {
 		log.Fatal().Msg(ErrLndNotFound.Error())
-		return ErrLndNotFound
+		return nil, ErrLndNotFound
 	}
 	// startup LND
 	cmd := exec.Command("lnd", "--bitcoin.simnet", "--bitcoin.active", "--bitcoin.node=btcd")
 	cmdReader, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal().Msg(fmt.Sprint(err))
+		return nil, err
 	}
 	scanner := bufio.NewScanner(cmdReader)
 	re := regexp.MustCompile(lndLogRegex)
 	wg.Add(1)
-	go parseLndLog(scanner, &log, re, shutdownInterceptor.ShutdownChannel(), wg)
+	go parseLndLog(scanner, log, re, shutdownInterceptor.ShutdownChannel(), wg)
 	if err := cmd.Start(); err != nil {
 		log.Fatal().Msg(fmt.Sprint(err))
+		return scanner, err
 	}
 	if err := cmd.Wait(); err != nil {
 		log.Fatal().Msg(fmt.Sprint(err))
+		return scanner, err
 	}
-	<-shutdownInterceptor.ShutdownChannel()
-	wg.Wait()
-	return nil
+	return scanner, nil
 }
